90b667e852c3effa55080ef659c75a9c18f17129
Jola <recka.jolanta@gmail.com>
2019-10-05 13:33:22 +0200

    container css

--- add-to-list/main.js
+++ add-to-list/main.js
@@ -2,23 +2,26 @@ let size = 10;
 let orderElement = 1;
 
 const init = () => {
+    const container = document.createElement('div');
     const button = document.createElement('button');
     const buttonReset = document.createElement('button');
     const list = document.createElement('ul');
     
-    document.body.appendChild(button);
-    document.body.appendChild(buttonReset);
-    document.body.appendChild(list);
+    document.body.appendChild(container);
+    container.appendChild(button);
+    container.appendChild(buttonReset);
+    container.appendChild(list);
 
     button.textContent = 'add 10 elements to the list';
     buttonReset.textContent = 'clean the list';
+    button.classList.add('button');
+    buttonReset.classList.add('buttonReset');
     button.addEventListener('click', createLiElements);
     buttonReset.addEventListener('click', cleanList);
 }
 
 const cleanList = () => {
     const list = document.querySelectorAll('li');
-    console.log('jest dobrze :D');
     list.forEach(element => {
         element.parentNode.removeChild(element);
         orderElement = 1;
