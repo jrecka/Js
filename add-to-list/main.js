4af4859c6fe0fc3f8494eed8302f212ac00452ec
Jola <recka.jolanta@gmail.com>
2019-10-07 22:54:39 +0200

    remove one button added

--- add-to-list/main.js
+++ add-to-list/main.js
@@ -5,23 +5,32 @@ const init = () => {
     const container = document.createElement('div');
     const button = document.createElement('button');
     const buttonReset = document.createElement('button');
+    const buttonRemove = document.createElement('button');
     const list = document.createElement('ul');
     
     document.body.appendChild(container);
     container.classList.add('container');
     list.classList.add('list-group');
     container.appendChild(button);
+    container.appendChild(buttonRemove);
     container.appendChild(buttonReset);
     container.appendChild(list);
 
     button.textContent = 'add';
     buttonReset.textContent = 'clean';
+    buttonRemove.textContent = '-1';
     button.classList.add('btn');
+    buttonRemove.classList.add('btn');
     buttonReset.classList.add('btn');
     button.addEventListener('click', createLiElements);
+    buttonRemove.addEventListener('click', removeOne);
     buttonReset.addEventListener('click', cleanList);
 }
-
+const removeOne = () => {
+    const list = document.querySelectorAll('li');
+    list[0].parentNode.removeChild(list[list.length - 1]);
+    orderElement--;  
+}
 const cleanList = () => {
     const list = document.querySelectorAll('li');
     list.forEach(element => {
