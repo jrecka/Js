038ad5a45e87eac4fd7cab4c60864811484c1719
Jola <recka.jolanta@gmail.com>
2019-10-10 12:25:07 +0200

    new

---
+++ color-change-key/main.js
@@ -0,0 +1,23 @@
+let red = 100;
+let green = 100;
+let blue = 100;
+
+document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+
+const changeColor = (e) => {
+  // console.log(e.keyCode, e.which);
+  if (e.keyCode === 38) {
+    red -=3;
+    green++;
+    blue +=2;
+    document.body.style.backgroundColor =`rgb(${red}, ${green}, ${blue})`;
+  } else if (e.keyCode === 40) {
+    red +=3;
+    green--;
+    blue -=2;
+    document.body.style.backgroundColor =`rgb(${red}, ${green}, ${blue})`;
+
+  }
+}
+
+window.addEventListener('keydown', changeColor);
