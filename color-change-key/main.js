0247b821ea135545570ef06b04935fe539874770
Jola <recka.jolanta@gmail.com>
2019-10-10 12:52:47 +0200

    max value added

--- color-change-key/main.js
+++ color-change-key/main.js
@@ -20,10 +20,16 @@ const changeColor = (e) => {
 
     switch ( e.keyCode ) {
       case 38:
-        document.body.style.backgroundColor = `rgb(${red++}, ${green++}, ${blue++})`;
+        document.body.style.backgroundColor = `rgb(
+          ${red <= 255 ? red++ : red}, 
+          ${green <= 255 ? green++ : green}, 
+          ${blue <= 255 ? blue++ : blue})`;
         break;
       case 40:
-        document.body.style.backgroundColor = `rgb(${red--}, ${green--}, ${blue--})`;
+        document.body.style.backgroundColor = `rgb(
+          ${red > 0 ? red-- : red}, 
+          ${green > 0 ? green-- : green}, 
+          ${blue > 0 ? blue-- : blue})`;
         break;
     }
 }
