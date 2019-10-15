8fcbd8ad8bf91bd20017559d3bcdc7029018915a
Jola <recka.jolanta@gmail.com>
2019-10-15 21:33:13 +0200

    toggle display

--- passoword/script.js
+++ passoword/script.js
@@ -3,14 +3,14 @@ const paragraph = document.querySelector('p');
 const passwords = ['password', 'pumpkin'];
 const messages = ['Every day is a new day', 'Happy Halloween'];
 
-paragraph.style.display = 'none';
-
 input.addEventListener('keyup', e => {
     const text = e.target.value;
     
+    paragraph.style.display = 'none';
     passwords.forEach( (password, index) => {
         if ( password === text ) {
-            paragraph.style.display = "block";
+            
+            paragraph.setAttribute('style', 'display:block !important');;
             paragraph.textContent = messages[index];
         }
     } )
