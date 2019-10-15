d7f482ea1a9b1930396e086a062e928925732ee2
Jola <recka.jolanta@gmail.com>
2019-10-15 21:11:31 +0200

    other password added

--- passoword/script.js
+++ passoword/script.js
@@ -1,12 +1,18 @@
 const input = document.getElementById('pass');
 const paragraph = document.querySelector('p');
-const password = 'password';
+const passwords = ['password', 'pumpkin'];
+const messages = ['Every day is a new day', 'Happy Halloween'];
 
 paragraph.style.display = 'none';
 
 input.addEventListener('keyup', e => {
-    password === e.target.value ?
-    paragraph.style.display = "block" :
-    paragraph.style.display = "none" 
-    }  
+    const text = e.target.value;
+    
+    passwords.forEach( (password, index) => {
+        if ( password === text ) {
+            paragraph.style.display = "block";
+            paragraph.textContent = messages[index];
+        }
+    } )
+}  
     );
