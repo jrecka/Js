fdd3499599d0df0b02bbf6583777358b76e44358
Jola <recka.jolanta@gmail.com>
2019-10-25 10:01:05 +0200

    ai choice added

--- rock-paper-scissors-game/script.js
+++ rock-paper-scissors-game/script.js
@@ -1,3 +1,5 @@
+const hands = [...document.querySelectorAll('.select img')];
+
 const gameSummary = {
     numbers: 0,
     wins: 0,
@@ -7,7 +9,7 @@ const gameSummary = {
 
 const game = {
     playerHand: "",
-    computerHand: "",
+    aiHand: "",
 }
 
 const handSelection = (e) => {
@@ -15,13 +17,20 @@ const handSelection = (e) => {
     game.playerHand = hand.dataset.option; 
     hands.forEach( hand => hand.style.boxShadow = '' );
     hand.style.boxShadow = '0 0 0 4px red';
+}
+ 
+const aiChoice = (e) => {
+    let index = Math.floor( Math.random() * 3 )
+    const aiHand = hands[index].dataset.option;
+    return aiHand;
+}
+
+const startGame = () => {
+    if ( !game.playerHand ) {
+        alert('Choose a hand!');
     }
-    
-// function handSelection() {
-//     console.log('lol');
-// game.playerHand = this.dataset.option;
-// console.log(game.playerHand );
-// }
-const hands = [...document.querySelectorAll('.select img')];
+    game.aiHand = aiChoice()
+}
 
 hands.forEach(hand => hand.addEventListener('click', handSelection));
+document.querySelector('.start').addEventListener('click', startGame)
