26cb3804ff94d91feb0134be552d0f3359d71dfd
Jola <recka.jolanta@gmail.com>
2019-11-05 12:16:23 +0100

    time counter added

--- counter/script.js
+++ counter/script.js
@@ -1,31 +1,10 @@
-// const add = ( start = 0) => {
-//     let number = start;
-//     return () => {
-//         start == 0 ? number++ : number+=start;
-        
-//         document.body.textContent = `aktualny stan licznika ${number}`;
-//     }
-// }
-// const counter = add();
-// const counterFrom5 = add(5);
-// counter;
-// document.addEventListener('click', counter);
-
-const user = (name ="", age) => {
-    let userName = name;
-    let userAge = age;
-
-    function showName () {
-    const paragraph = document.createElement('p');
-    document.body.appendChild(paragraph);
-    paragraph.textContent = `Hello ${userName}, ${userAge >= 18 ? 'you are adult' : 'you are not adult'}`;
+const counter = () => {
+    let number  = 0;
+    const  print =  () => {
+        number++;
+        document.body.textContent = `You are ${number} seconds on this page`;
+    }
+    return print
 }
-return showName
-}
-
-const john = user('John', 22);
-const mary = user('Mary', 30);
-const linda = user('Linda', 12);
-john();
-mary();
-linda();
+const start = counter();
+setInterval(start, 1000);
