a7ec512f126c53b8c05acaa76725d91e15c8ea1c
Jola <recka.jolanta@gmail.com>
2019-11-05 11:51:59 +0100

    demo function

--- counter/script.js
+++ counter/script.js
@@ -1,12 +1,31 @@
-const add = ( start = 0) => {
-    let number = start;
-    return () => {
-        start == 0 ? number++ : number+=start;
+// const add = ( start = 0) => {
+//     let number = start;
+//     return () => {
+//         start == 0 ? number++ : number+=start;
         
-        document.body.textContent = `aktualny stan licznika ${number}`;
-    }
+//         document.body.textContent = `aktualny stan licznika ${number}`;
+//     }
+// }
+// const counter = add();
+// const counterFrom5 = add(5);
+// counter;
+// document.addEventListener('click', counter);
+
+const user = (name ="", age) => {
+    let userName = name;
+    let userAge = age;
+
+    function showName () {
+    const paragraph = document.createElement('p');
+    document.body.appendChild(paragraph);
+    paragraph.textContent = `Hello ${userName}, ${userAge >= 18 ? 'you are adult' : 'you are not adult'}`;
 }
-const counter = add();
-const counterFrom5 = add(5);
-counter;
-document.addEventListener('click', counter);
+return showName
+}
+
+const john = user('John', 22);
+const mary = user('Mary', 30);
+const linda = user('Linda', 12);
+john();
+mary();
+linda();
