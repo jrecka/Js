ddb7c7d6ce57dcb55cf7ce889431e87175ad6eb9
Jola <recka.jolanta@gmail.com>
2019-11-18 19:06:24 +0100

    adding tasks added

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,19 +1,37 @@
 const input = document.querySelector('input');
+const form = document.querySelector('form');
 const list = document.querySelector('ul');
-const listBtn = document.querySelectorAll('li button');
-const listElement = document.querySelectorAll('li');
+const listElements  = document.getElementsByTagName('li');
+const taskNumber = document.querySelector('h1 span');
+const taskNumberUpdate = () => taskNumber.textContent = listElements.length;
 
-const removeElement = e => 
+const removeElement = e => {
     e.target.parentNode.remove();
+    taskNumberUpdate();
+}
 const crossOutElement = e => 
     e.target.classList.toggle('cross-out');
+
 const searchTask = e => {
     const searchTxt = e.target.value.toLowerCase();
-    let tasks = [...listElement];
+    let tasks = [...listElements];
     tasks = tasks.filter( li => li.textContent.toLocaleLowerCase().includes(searchTxt));
     list.textContent = "";
     tasks.forEach(li => list.appendChild(li));
+    taskNumberUpdate();
 }
-input.addEventListener('input', searchTask);
-listElement.forEach( item => item.addEventListener('click', crossOutElement)); 
-listBtn.forEach( item => item.addEventListener('click', removeElement));
+const addTask = e => {
+    e.preventDefault();
+    const titleTask = input.value;
+    if( titleTask === "" ) return;
+    const task = document.createElement('li');
+    task.innerHTML = titleTask + '<button>x</button>';
+    list.appendChild(task);
+    input.value = "";
+    taskNumberUpdate();
+    task.querySelector('button').addEventListener('click', removeElement);
+    task.addEventListener('click', crossOutElement);
+}
+
+form.addEventListener('submit', addTask);
+input.addEventListener('blur', searchTask);
